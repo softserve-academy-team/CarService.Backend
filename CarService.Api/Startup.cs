a8ca8b44816b4f6282f67981ad6ad3ad6b9185e3
Oleh Dovhan <oleh.dovhan.731@outlook.com>
2018-01-26 16:36:05 +0200

    Fixed error with CORS

--- CarService.Api/Startup.cs
+++ CarService.Api/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
 using Microsoft.Extensions.DependencyInjection;
 using CarService.Api.Services;
 
@@ -10,8 +12,13 @@ namespace CarService.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
             services.AddMvc();
+            services.AddCors(options => {
+                options.AddPolicy("AllowAllOrigin", builder => builder.AllowAnyOrigin());
+            });
+            services.Configure<MvcOptions>(options => {
+                options.Filters.Add(new CorsAuthorizationFilterFactory("AllowAllOrigin"));
+            });
 
             services.AddSingleton<ICarUrlBuilder, AutoRiaCarUrlBuilder>();
             services.AddSingleton<ICarMapper, AutoRiaCarMapper>();            
@@ -21,8 +28,6 @@ namespace CarService.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ICarService carService)
         {
-            app.UseCors(builder => builder.AllowAnyOrigin());
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
