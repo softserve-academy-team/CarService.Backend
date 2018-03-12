9deb6e1269f824521f19b23a04c7ae8c8af6185b
olegbondur <olegbondur@gmail.com>
2018-03-12 11:15:28 +0200

    add XML describe headers to Order controller

--- CarService.Api/Controllers/OrdersController.cs
+++ CarService.Api/Controllers/OrdersController.cs
@@ -37,12 +37,24 @@ namespace CarService.Api.Controllers
             return Ok();
         }
 
+        /// <summary>Get cityes with auto in stock</summary>
+        /// <returns>an IEnumerable</returns>
+        /// <remarks>Get cityes with auto in stock</remarks>
+        /// <response code="200">Get cityes successful</response>
+        /// <response code="401">Unauthorized user</response> 
+        /// <response code="500">Internal Server Error</response> 
         [HttpGet("cities")]
         public async Task<IEnumerable<string>> GetCities()
         {
             return await _orderService.GetCitiesAsync();
         }
 
+        /// <summary>Get orders</summary>
+        /// <returns>an IEnumerable</returns>
+        /// <remarks>Get orders</remarks>
+        /// <response code="200">Get orders successful</response>
+        /// <response code="401">Unauthorized user</response> 
+        /// <response code="500">Internal Server Error</response> 
         [HttpPost]
         public async Task<IEnumerable<BaseOrderInfo>> GetOrders([FromBody]OrderSearchModel orderSearchModel, [FromQuery(Name = "skip")]int skip, [FromQuery(Name = "take")]int take)
         {
