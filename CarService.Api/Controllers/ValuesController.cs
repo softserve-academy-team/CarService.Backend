d4c826f8712c4f0ee7d44eb43202b70796683a62
olegbondur <olegbondur@gmail.com>
2018-03-15 17:53:00 +0200

    test database azure

--- CarService.Api/Controllers/ValuesController.cs
+++ CarService.Api/Controllers/ValuesController.cs
@@ -11,6 +11,7 @@ namespace CarService.Api.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
